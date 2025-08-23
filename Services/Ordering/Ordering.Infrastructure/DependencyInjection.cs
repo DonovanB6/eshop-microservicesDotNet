e74b6c539724edc3cbe8cb748f7eafbbe4deb313
Donovan <itsdcb11@verizon.net>
2025-08-22 20:02:40 -0400

    Adds Ordering database and infrastructure
    Configures the Ordering service to use a SQL Server database
    and integrates Entity Framework Core for data access.
    
    Includes initial database schema, configurations,
    seeding, and interception of domain events.

--- Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Data.Interceptors;
 
 namespace Ordering.Infrastructure
 {
@@ -10,9 +14,16 @@ namespace Ordering.Infrastructure
             var connectionString = configuration.GetConnectionString("Database");
 
             /// Add services to the container
-            /// services.AddDbContext<ApplicationDbContext>(options =>
-            /// options.UseSqlService(connectionstring));
-            ///services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString);
+            });
+
+            // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
             return services;
         }
