05776166a6b203a47452802700286593b14ec77f
Donovan <itsdcb11@verizon.net>
2026-01-10 17:59:39 -0500

    Created Ordering CreateOrder Command

---
+++ Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order): ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor( x => x.Order.OrderName).NotEmpty().WithMessage("Name is Required");
+        RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("CustomerId is Required");
+        RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems is Required");
+    }
+}
--- Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
 using Ordering.Domain.Models;
 using System.Reflection;
 
 namespace Ordering.Infrastructure.Data
 {
-    internal class ApplicationDbContext : DbContext
+    internal class ApplicationDbContext : DbContext, IApplicationDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
